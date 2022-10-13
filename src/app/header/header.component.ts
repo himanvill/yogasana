261aff319810d311412f87ce2ec45cb460634308
Himanvill <himanvill@gmail.com>
2022-10-13 23:09:17 +0530

    Add Routing navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  bgClass: string = 'background';
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const eventUrl = event.urlAfterRedirects;
+        console.log(eventUrl);
+        console.log(this.bgClass);
+        if (eventUrl.split('/')[1] != 'home') {
+          this.bgClass = 'no-home';
+          console.log(this.bgClass);
+        } else {
+          this.bgClass = 'background';
+        }
+        console.log(this.bgClass);
+      }
+    });
   }
 
+  ngOnInit(): void {}
 }
