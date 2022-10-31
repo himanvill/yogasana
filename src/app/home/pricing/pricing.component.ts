994e91cfaf33f316d043f0921b0e9728f810f28b
Himanvill <himanvill@gmail.com>
2022-10-31 14:47:12 +0530

    Pricing component

--- src/app/home/pricing/pricing.component.ts
+++ src/app/home/pricing/pricing.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.css']
+  styleUrls: ['./pricing.component.css'],
 })
 export class PricingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  bgClass: string = 'background';
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const eventUrl = event.urlAfterRedirects;
+        console.log(eventUrl);
+        console.log(this.bgClass);
+        if (eventUrl.split('/')[1] != 'home') {
+          this.bgClass = 'no-home';
+          console.log(this.bgClass);
+        } else {
+          this.bgClass = 'background';
+        }
+        console.log(this.bgClass);
+      }
+    });
   }
 
+  ngOnInit(): void {}
 }
